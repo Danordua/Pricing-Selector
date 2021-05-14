d9c75ebf4a6bec17d104e22d89442ffa2e88ac0a
Daniel Orduña <danordua@gmail.com>
2021-05-13 21:51:26 -0500

    JS V.2 funciona

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const level = document.querySelector('#range');
 const numPages = document.querySelector('.value-num');
 const price = document.querySelector('.price-num');
+const switchBtn = document.querySelector('#switch');
 
+// cambia los numeros de pages
 numPages.textContent = level.value;
+switchBtn.value = 'off';
+
+// cambia los precios con descuento
+switchBtn.addEventListener('click', () => {
+    // combierte el precio (string) a un número
+    const priceDis = Number(price.textContent);
+    //dependiendo del valor del switch cambia el valor 
+    if(switchBtn.value === "on") {
+         switchBtn.value = "off";
+         // lo revierte al valor original
+         price.textContent = Math.floor(level.value / 10);
+    }
+    else {
+        switchBtn.value = "on";
+        //valor menos el descuento
+        price.textContent = Math.floor(priceDis - (priceDis * 0.25));
+    }
+})
 
 level.oninput = function(){
     numPages.textContent = this.value;
+    price.textContent = Math.floor(this.value / 10);
+    // cambia el valor si el switch está activado
+    if(switchBtn.value == 'on') price.textContent = Math.floor((this.value / 10) - ((this.value / 10) * 0.25));
+    if(this.value <= 100) price.textContent = 00;
 };
 
-if(this.value > 100 && this.value < 200) price.textContent = '10';
-    if(this.value > 200 && this.value < 300) price.textContent = '10';
+
+
 
